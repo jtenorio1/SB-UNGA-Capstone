dc46e5509e3d241d6c1654cdd614c3904f262d8d
jtenorio1 <jtenori1@gmail.com>
2016-12-05 00:31:17 -0700

    Add files via upload

--- P2 UN Exploratory Analysis.R
+++ P2 UN Exploratory Analysis.R
@@ -59,6 +59,51 @@ str(voteinfo)
 
 
 
+#Adding & Manipulating Data (column changes)
+#----------------------------------------------------------------------------------------------------------------
+#voteinfo
+#votes are passed by a two thirds majority so we need to add a few columns
+#Add column for total number of countries voting
+voteinfo <- voteinfo %>% mutate(sum.votes = abstain + yes + no)
+#reorder
+voteinfo <- voteinfo[, c(1,2,3,4,5,6,7,19,8:ncol(voteinfo))]
+#percent abstain, yes, and no
+voteinfo <- voteinfo %>% mutate(p.abstain = abstain/sum.votes)
+voteinfo <- voteinfo %>% mutate(p.yes = yes/sum.votes)
+voteinfo <- voteinfo %>% mutate(p.no = no/sum.votes)
+#vote adopt/reject
+voteinfo <- voteinfo %>% mutate(vote.result = ifelse(p.yes >= (2/3), 1, 0)) #1 = ADOPT & 0 = REJECT
+#reorder
+voteinfo <- voteinfo[, c(1,2,3,4,5,6,7,8, 21,22,23,24, 9:ncol(voteinfo))]
+#cleanup: drop unnecessary columns
+drops <- c(2,24,25,26,27,28)
+voteinfo <- voteinfo[,-drops]
+voteinfo <- voteinfo[,-1]
+
+write.csv(voteinfo, file = "voteinfo")
+
+#countrycodes
+#drop duplicate identifier col
+countrycodes <- countrycodes[, -c(1,2)]
+write.csv(countrycodes, file = "countrycodes")
+
+
+#countryinfo
+#drop duplicate identifier cols
+countryinfo <- countryinfo[, -c(1,2)]
+write.csv(countryinfo, file = "countryinfo")
+
+#ideals
+#drop duplicate ID cols & n_full col
+ideals <- ideals[, -c(1,2,7)]
+write.csv(ideals, file = "ideals")
+
+#rawvotes
+#drop duplicate ID col
+rawvotes <- rawvotes[,-c(1,2)]
+write.csv(rawvotes, file = "rawvotes")
+
+
 #Data Plotting Below 
 #----------------------------------------------------------------------------------------------------------------
 
